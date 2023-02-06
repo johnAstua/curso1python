58d908e1d6ea98e23df8b73e38563ce9d8acb579
johnAstua <johnastualopez2303@gmail.com>
2023-02-05 20:47:52 -0600

    Revert "Revert "Revert "Merge branch 'tarea-3-pruebas'"""
    This reverts commit 5c227b31425fa633c073810d3bf991aa71dad0e2.

---
+++ tarea-2/problema_1.py
@@ -0,0 +1,16 @@
+#problema 1 sacar factorial
+
+#crear una variable, convertirla en entero
+numero = int(input("numero:"))
+factorial = 1         #guardar factorial del numero, iniciar en 1
+if numero != 0:       #preguntar si el numero encontrado es diferente a 0
+    for i in range(1, numero + 1): #va empezar desde 1 hasta el numero ingresado + 1 porque estaria por debajo
+        factorial = factorial * i #factorial = a factorial pero multiplicado por i
+
+if numero < 0:  #si el numero es menor que cero dara error
+    print("error")
+
+if numero > 0 :
+    print("factorial:",factorial)
+
+
